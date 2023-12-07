fa804d1a3415752ad183f468d6bea8cd42228f9c
Kushal Agarwalla <kushal.agarwal72@gmail.com>
2023-12-07 12:42:37 +0530

    Time: 29 ms (25.36%) | Memory: 33 MB (34.23%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* minVal(TreeNode* root)
+    {
+        TreeNode* temp=root;
+        while(temp->left!=NULL)
+            temp=temp->left;
+        return temp;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        //base case
+        if(root==NULL)
+            return NULL;
+        if(root->val==key)
+        {
+            //0 child
+            if(root->left==NULL && root->right==NULL)
+            {
+                delete root;
+                return NULL;
+            }
+
+            //1 child
+            //left child
+            if(root->left!=NULL && root->right==NULL)
+            {
+                TreeNode* temp=root->left;
+                delete root;
+                return temp;
+            }
+            //right child
+            if(root->left==NULL && root->right!=NULL)
+            {
+                TreeNode* temp=root->right;
+                delete root;
+                return temp;
+            }
+            //2 child
+            if(root->left!=NULL && root->right!=NULL)
+            {
+                int mini=minVal(root->right)->val;
+                root->val=mini;
+                root->right=deleteNode(root->right,mini);
+                return root;
+            }
+        }
+        if(root->val>key)
+        {
+            root->left= deleteNode(root->left,key);
+            return root;
+        }
+        else
+        {
+            root->right= deleteNode(root->right,key);
+            return root;
+        }
+    }
+};
