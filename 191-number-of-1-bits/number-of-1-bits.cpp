dd12da55235baab17cbd201c72681ab481034ec0
Kushal Agarwalla <kushal.agarwal72@gmail.com>
2024-09-12 11:28:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (78.69%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -4,9 +4,9 @@ public:
         int count=0;
         while(n!=0)
         {
-            if(n&1)
+            if(n&1==1)
                 count++;
-           n= n>>1;
+            n= n>>1;
         }
         return count;
     }
